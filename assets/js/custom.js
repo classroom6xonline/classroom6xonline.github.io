3d7349c26fc590555ce2775c08b40551ea5b6459
junaid <mirzajunaid647@gmail.com>
2026-08-27 14:46:05 +0500

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,3 @@
-(function () {
-    const script = document.createElement('script');
-
-    script.async = true;
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5305579698272359';
-    script.crossOrigin = 'anonymous';
-
-    document.head.appendChild(script);
-})();
-
-
-
 
 $(document).ready(function() {
     $(window).scroll(function() {
